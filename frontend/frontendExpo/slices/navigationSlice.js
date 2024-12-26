ce1e80e8390970d3f60957523dd45445adc0b1e1
Anish0999 <anishgupta0999@gmail.com>
2024-12-26 10:18:10 -0500

    Initial Redux Setup

---
+++ frontend/frontendExpo/slices/navigationSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    pickupDestination: null,
+    dropoffDestination: null,
+    travelTimeInformation: null
+}
+
+export const navigationSlice = createSlice({
+    name: 'navigation',
+    initialState,
+    reducers: {
+        setOrigin: (state, action) => {
+            state.origin = action.origin;
+        },
+        setPickupDestination: (state, action) => {
+            state.pickupDestination = action.origin;
+        },
+        setDropoffDestination: (state, action) => {
+            state.dropoffDestination = action.origin;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        }
+    }
+})
+
+// Actions (Setters)
+export const { setOrigin, setPickupDestination, setDropoffDestination, setTravelTimeInformation } = createSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.navigation.origin;
+export const selectPickupDestination = (state) => state.navigation.pickupDestination;
+export const selectDropoffDestination = (state) => state.navigation.dropoffDestination;
+export const selectTravelTimeInformation = (state) => state.navigation.travelTimeInformation;
+
+export default navigationSlice.reducer;
